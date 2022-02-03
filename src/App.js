d53e9a6b9b84dd7d3f8041ae437b1e5ca8d2e5e8
figo5530 <figooooo0221@gmail.com>
2022-02-03 17:51:51 -0500

    sets up scss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div>
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+    </ul>
+  </div>;
 }
 
-export default App;
