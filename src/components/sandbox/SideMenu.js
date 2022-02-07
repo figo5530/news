13153c48929e57e1f11e85800a9aa7ac215ee505
figo5530 <figooooo0221@gmail.com>
2022-02-07 13:18:07 -0500

    mimicks dynamically getting side menu list and rendering

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -2,35 +2,67 @@ import React from 'react';
 import { Layout, Menu } from 'antd'
 import {
   UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
   SettingOutlined
 } from '@ant-design/icons';
 import './SideMenu.css'
+import MenuItem from 'antd/lib/menu/MenuItem';
 const { Sider } = Layout
 const { SubMenu } = Menu
+const menuList = [
+  {
+    key: '/home',
+    title: 'Home',
+    icon: <UserOutlined />
+  },
+  {
+    key: '/user-manage',
+    title: 'User Administration',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        key: '/user-manage/list',
+        title: 'User List',
+        icon: <UserOutlined />
+      },
+    ]
+  },
+  {
+    key: '/right-manage',
+    title: 'Right Administration',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        key: '/right-manage/role/list',
+        title: 'Role List',
+        icon: <UserOutlined />
+      },
+      {
+        key: '/right-manage/right/list',
+        title: 'Right List',
+        icon: <UserOutlined />
+      },
+    ]
+  }
+]
 
 export default function SideMenu() {
+  const renderMenu = (list) => {
+    return list.map(item => {
+      if(item.children) {
+        return <SubMenu key={item.key} title={item.title} icon={item.icon}>
+                  {renderMenu(item.children)}
+              </SubMenu>
+      }else {
+        return <MenuItem key={item.key} icon={item.icon}>{item.title}</MenuItem>
+      }
+    })
+  }
   
   return (
     <Sider trigger={null} collapsible collapsed={false}>
     <div className="logo">News System</div>
     <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-      <Menu.Item key="1" icon={<UserOutlined />}>
-        Home
-      </Menu.Item>
-      <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-        nav 2
-      </Menu.Item>
-      <Menu.Item key="3" icon={<UploadOutlined />}>
-        nav 3
-      </Menu.Item>
-      <SubMenu key="sub4" icon={<SettingOutlined />} title="User Administration">
-          <Menu.Item key="9">Option 9</Menu.Item>
-          <Menu.Item key="10">Option 10</Menu.Item>
-          <Menu.Item key="11">Option 11</Menu.Item>
-          <Menu.Item key="12">Option 12</Menu.Item>
-      </SubMenu>
+      {renderMenu(menuList)}
     </Menu>
   </Sider>
   )
