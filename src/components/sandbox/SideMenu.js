3a29e9085ece3b80e11951f2dc18de624fb5bbca
figo5530 <figooooo0221@gmail.com>
2022-02-08 20:58:28 -0500

    replaces sideMenu api data with json-server data

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,54 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout, Menu } from 'antd'
-import {
-  UserOutlined,
-  SettingOutlined
-} from '@ant-design/icons';
+// import {
+//   UserOutlined,
+//   SettingOutlined
+// } from '@ant-design/icons';
 import './SideMenu.css'
 import { useNavigate } from 'react-router-dom'
 import MenuItem from 'antd/lib/menu/MenuItem';
+import axios from 'axios'
 
 const { Sider } = Layout
 const { SubMenu } = Menu
-const menuList = [
-  {
-    key: '/home',
-    title: 'Home',
-    icon: <UserOutlined />
-  },
-  {
-    key: '/user-manage',
-    title: 'User Administration',
-    icon: <SettingOutlined />,
-    children: [
-      {
-        key: '/user-manage/list',
-        title: 'User List',
-        icon: <UserOutlined />
-      },
-    ]
-  },
-  {
-    key: '/right-manage',
-    title: 'Right Administration',
-    icon: <SettingOutlined />,
-    children: [
-      {
-        key: '/right-manage/role/list',
-        title: 'Role List',
-        icon: <UserOutlined />
-      },
-      {
-        key: '/right-manage/right/list',
-        title: 'Right List',
-        icon: <UserOutlined />
-      },
-    ]
-  }
-]
+// const menuList = [
+//   {
+//     key: '/home',
+//     title: 'Home',
+//     icon: <UserOutlined />
+//   },
+//   {
+//     key: '/user-manage',
+//     title: 'User Administration',
+//     icon: <SettingOutlined />,
+//     children: [
+//       {
+//         key: '/user-manage/list',
+//         title: 'User List',
+//         icon: <UserOutlined />
+//       },
+//     ]
+//   },
+//   {
+//     key: '/right-manage',
+//     title: 'Right Administration',
+//     icon: <SettingOutlined />,
+//     children: [
+//       {
+//         key: '/right-manage/role/list',
+//         title: 'Role List',
+//         icon: <UserOutlined />
+//       },
+//       {
+//         key: '/right-manage/right/list',
+//         title: 'Right List',
+//         icon: <UserOutlined />
+//       },
+//     ]
+//   }
+// ]
 
 function SideMenu(props) {
+  
   let navigate = useNavigate()
+
+  const [menu, setMenu] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/rights?_embed=children").then(res => {
+      console.log(res.data)
+      setMenu(res.data)
+    })
+  }, [])
+
   const renderMenu = (list) => {
     return list.map(item => {
       if(item.children) {
@@ -67,7 +79,7 @@ function SideMenu(props) {
     <Sider trigger={null} collapsible collapsed={false}>
     <div className="logo">News System</div>
     <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-      {renderMenu(menuList)}
+      {renderMenu(menu)}
     </Menu>
   </Sider>
   )
