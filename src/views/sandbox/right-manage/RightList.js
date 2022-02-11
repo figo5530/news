69cc7ed562fcef6a93caacaaaf899fb16bf908e5
figo5530 <figooooo0221@gmail.com>
2022-02-10 23:38:42 -0500

    adds table component from antd to load right list from backend basically

--- src/views/sandbox/right-manage/RightList.js
+++ src/views/sandbox/right-manage/RightList.js
@@ -1,5 +1,43 @@
-import React from 'react';
+import { Table } from 'antd';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 export default function RightList() {
-  return <div>RightList</div>;
+
+  const [dataSource, setDataSource] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/rights").then(res => {
+      setDataSource(res.data)
+    })
+  },[])
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Right Name',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      title: 'Right Path',
+      dataIndex: 'key',
+      key: 'key',
+    },
+    // {
+    //   title: 'Operation',
+    //   dataIndex: 'address',
+    //   key: 'address',
+    // },
+  ];
+
+  return (
+    <div>
+      <Table dataSource={dataSource} columns={columns} />;
+    </div>
+  )
 }
