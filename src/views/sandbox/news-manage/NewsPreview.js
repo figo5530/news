2c563ee1691c59b40c6e67d44889d1d1758ee3e8
figo5530 <figooooo0221@gmail.com>
2022-02-24 15:58:06 -0500

    adds pageHeader content rendering based on the new info we get back from backend

--- src/views/sandbox/news-manage/NewsPreview.js
+++ src/views/sandbox/news-manage/NewsPreview.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { PageHeader, Descriptions } from 'antd';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
-
+import moment from 'moment'
 
 export default function NewsPreview() {
 
@@ -10,30 +10,35 @@ export default function NewsPreview() {
   const [newsInfo, setNewsInfo] = useState(null)
   useEffect(() => {
     axios.get(`http://localhost:5000/news/${location.pathname.split("/")[4]}?_expand=category&_expand=role`).then(res => {
+      console.log(res.data)
       setNewsInfo(res.data)
     })
   }, [location.pathname])
 
+  const auditList = ["Unaudited", "Audited", "Approved", "Disapproved"]
+  const publishList = ["Unpublished", "Waiting", "Published", "Archived"]
   return (
     <div>
-      <PageHeader
-        onBack={() => window.history.back()}
-        title="Title"
-        subTitle="This is a subtitle"
-      >
-        <Descriptions size="small" column={3}>
-          <Descriptions.Item label="Creator">Lili Qu</Descriptions.Item>
-          <Descriptions.Item label="Creation Time">2017-01-10</Descriptions.Item>
-          <Descriptions.Item label="Publish Time">2017-10-10</Descriptions.Item>
-          <Descriptions.Item label="Region">2017-10-10</Descriptions.Item>
-          <Descriptions.Item label="Audit Status">2017-10-10</Descriptions.Item>
-          <Descriptions.Item label="Publish Status">2017-10-10</Descriptions.Item>
-          <Descriptions.Item label="Viewd">2017-10-10</Descriptions.Item>
-          <Descriptions.Item label="Stars">2017-10-10</Descriptions.Item>
-          <Descriptions.Item label="Comments">2017-10-10</Descriptions.Item>
-          
-        </Descriptions>
-      </PageHeader>
+      {
+        newsInfo && <PageHeader
+          onBack={() => window.history.back()}
+          title={newsInfo.title}
+          subTitle={newsInfo.category.title}
+        >
+          <Descriptions size="small" column={3}>
+            <Descriptions.Item label="Creator">{newsInfo?.author}</Descriptions.Item>
+            <Descriptions.Item label="Creation Time">{moment(newsInfo.createTime).format("MM/DD/YYYY HH:mm:ss")}</Descriptions.Item>
+            <Descriptions.Item label="Publish Time">{newsInfo.publishTime ? moment(newsInfo.createTime).format("MM/DD/YYYY HH:mm:ss") : "-"}</Descriptions.Item>
+            <Descriptions.Item label="Region">{newsInfo.region}</Descriptions.Item>
+            <Descriptions.Item label="Audit Status"><span style={{ color: "red" }}>{auditList[newsInfo.auditState]}</span></Descriptions.Item>
+            <Descriptions.Item label="Publish Status"><span style={{ color: "red" }}>{publishList[newsInfo.publishState]}</span></Descriptions.Item>
+            <Descriptions.Item label="Viewed">{newsInfo.view}</Descriptions.Item>
+            <Descriptions.Item label="Stars">{newsInfo.star}</Descriptions.Item>
+            <Descriptions.Item label="Comments">0</Descriptions.Item>
+
+          </Descriptions>
+        </PageHeader>
+      }
     </div>
   )
 }
