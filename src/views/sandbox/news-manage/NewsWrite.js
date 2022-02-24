eb83f666a79f801a079391dfa4ec2dd00c5bd059
figo5530 <figooooo0221@gmail.com>
2022-02-23 19:56:42 -0500

    adds step 1 form and dynamically loads select options from backend

--- src/views/sandbox/news-manage/NewsWrite.js
+++ src/views/sandbox/news-manage/NewsWrite.js
@@ -1,17 +1,29 @@
-import React from 'react'
-import { Button, PageHeader, Steps } from 'antd'
+import React, { useEffect } from 'react'
+import { Button, PageHeader, Steps, Form, Input, Select } from 'antd'
 import { useState } from 'react'
+import style from './News.module.css'
+import axios from 'axios'
 
 export default function NewsWrite() {
 
+  const { Option } = Select
   const { Step } = Steps
-  const [currentStep, setCurrentStep] = useState(2)
+  const [currentStep, setCurrentStep] = useState(0)
+  const [categoryList, setCategoryList] = useState([])
+
   const handleNext = () => {
     setCurrentStep(currentStep + 1)
   }
+
   const handlePrevious = () => {
     setCurrentStep(currentStep - 1)
   }
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/categories").then(res => {
+      setCategoryList(res.data)
+    }, [])
+  })
   return (
     <div>
       <PageHeader
@@ -26,7 +38,61 @@ export default function NewsWrite() {
         <Step title="Submit" description="Draft or Audit" />
       </Steps>,
 
-      <div style={{marginTop: '50px'}}>
+      <div style={{ marginTop: "50px"}}>
+        <div className={currentStep === 0 ? '' : style.active}>
+          <Form
+            name="basic"
+            labelCol={{
+              span: 2,
+            }}
+            wrapperCol={{
+              span: 22,
+            }}
+            labelAlign="left"
+          // initialValues={{
+          //   remember: true,
+          // }}
+          // autoComplete="off"
+          >
+            <Form.Item
+              label="Title"
+              name="title"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the title!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Category"
+              name="categoryId"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please select the category!',
+                },
+              ]}
+            >
+              <Select>
+                {
+                  categoryList.map(item => {
+                    return <Option key={item.id} value={item.id}>{item.title}</Option>
+                  })
+                }
+              </Select>
+            </Form.Item>
+
+          </Form>
+        </div>
+        <div className={currentStep === 1 ? '' : style.active}><input /></div>
+        <div className={currentStep === 2 ? '' : style.active}><input /></div>
+      </div>
+
+      <div style={{ marginTop: '50px' }}>
         {
           currentStep === 2 && <span>
             <Button type='primary'>Save in Draft</Button>
