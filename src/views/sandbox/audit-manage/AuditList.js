b78ed0c829643616c335d74a3d68552c901cd30d
figo5530 <figooooo0221@gmail.com>
2022-02-26 22:10:47 -0500

    adds basic audit list table

--- src/views/sandbox/audit-manage/AuditList.js
+++ src/views/sandbox/audit-manage/AuditList.js
@@ -1,7 +1,61 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Button, Table } from 'antd'
 
 export default function AuditList() {
+
+  const { username } = JSON.parse(localStorage.getItem("token"))
+  const [dataSource, setDataSource] = useState([])
+  useEffect(() => {
+    axios.get(`http://localhost:5000/news?author=${username}&auditState_ne=0&publishState_lte=1&_expand=category`).then(res => {
+      setDataSource(res.data)
+    })
+  }, [username])
+
+  const columns = [
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      key: 'title',
+      render: (title, item) => {
+        return <a href={`#/news-manage/news/preview/${item.id}`}>{title}</a>
+      }
+    },
+    {
+      title: 'Author',
+      dataIndex: 'author',
+      key: 'author',
+    },
+    {
+      title: 'Catrgory',
+      dataIndex: 'category',
+      key: 'categoryId',
+      render: category => {
+        return category.title
+      }
+    },
+    {
+      title: 'Audit State',
+      dataIndex: 'auditState',
+      key: 'auditState',
+      render: auditState => {
+        return <div>{auditState}</div>
+      }
+    },
+    {
+      title: 'Operation',
+      render: (item) => {
+        return (
+          <div>
+            <Button type='primary'>Publish</Button>
+          </div>
+        )
+      }
+    }
+  ]
   return (
-    <div>AuditList</div>
+    <div>
+      <Table dataSource={dataSource} columns={columns} rowKey={item => item.id} pagination={{ pageSize: 5 }}></Table>
+    </div>
   )
 }
