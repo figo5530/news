dfeebc4a331d183a32d3d90b2a544690881b62e1
figo5530 <figooooo0221@gmail.com>
2022-03-04 16:03:06 -0500

    creates basic home layouts

--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -1,10 +1,53 @@
 import React from 'react';
-import { Button } from 'antd'
+import { Card, Col, Row, List, Avatar } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+const { Meta } = Card
 
 export default function Home() {
+
   return (
-    <div>
-      <Button type='primary'>Button</Button>
+    <div className="site-card-wrapper">
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card title="Most Viewed" bordered={true}>
+            <List
+              size="small"
+              dataSource={["111", "222"]}
+              renderItem={item => <List.Item>{item}</List.Item>}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Most Liked" bordered={true}>
+            <List
+              size="small"
+              dataSource={["111", "222"]}
+              renderItem={item => <List.Item>{item}</List.Item>}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            cover={
+              <img
+                alt="example"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+            }
+            actions={[
+              <SettingOutlined key="setting" />,
+              <EditOutlined key="edit" />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <Meta
+              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+              title="Card title"
+              description="This is the description"
+            />
+          </Card>
+        </Col>
+      </Row>
     </div>
   )
 }
