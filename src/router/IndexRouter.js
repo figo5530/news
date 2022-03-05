da01913c833ccf07e7a73a845692b584eaa4213f
figo5530 <figooooo0221@gmail.com>
2022-03-04 20:47:19 -0500

    adds router configuration for tourist mode

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Login from '../views/login/Login'
 import NewsSandBox from '../views/sandbox/NewsSandBox'
+import News from '../views/news/News'
+import Detail from '../views/news/Detail'
 
 export default function IndexRouter() {
   return (
     <HashRouter>
         <Routes>
             <Route path='/login' element={<Login />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/detail/:id' element={<Detail />} />
             {/* <Route path='/' element={ localStorage.getItem("token") ? <NewsSandBox /> : <Navigate to='/login' /> } /> */}
             <Route path='/*' element={ localStorage.getItem("token") ? <NewsSandBox/> : <Navigate to='/login' /> } />
             <Route path='*' element={<Navigate to='/login' />} />
