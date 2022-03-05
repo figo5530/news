29e34313ef341f415d46229e3e4b5343a5aac0a6
figo5530 <figooooo0221@gmail.com>
2022-03-05 15:39:52 -0500

    fills news component page with real data

--- src/views/news/News.js
+++ src/views/news/News.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import { PageHeader, Card, Col, Row, List } from 'antd'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import _ from 'lodash'
 
 function News() {
-  return (
-    <div>News</div>
-  )
+
+    const [list, setList] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/news?publishState=2&_expand=category").then(res => {
+            const data = Object.entries(_.groupBy(res.data, item => item.category.title))
+            setList(data)
+        })
+    }, [])
+
+    return (
+        <div style={{ width: "95%", margin: "0 auto" }}>
+            <PageHeader
+                className='site-page-header'
+                title="News Home"
+                subTitle="Extra!!"
+            />
+            <div className="site-card-wrapper">
+                <Row gutter={[16, 16]}>
+                    {list.map(item => {
+                        return (
+                            <Col span={8} key={item[0]}>
+                                <Card title={item[0]} bordered={true} hoverable={true} >
+                                    <List
+                                        size="small"
+                                        bordered
+                                        dataSource={item[1]}
+                                        renderItem={news => <List.Item><a href={`#/detail/${news.id}`}>{news.title}</a></List.Item>}
+                                        pagination={{ pageSize: 3 }}
+                                    />
+                                </Card>
+                            </Col>
+                        )
+                    })}
+                </Row>
+            </div>
+        </div>
+    )
 }
 
 export default News
